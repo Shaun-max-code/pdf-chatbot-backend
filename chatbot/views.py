8933c8f3dae34576f249c1919d3ec49e55452f31
Shaun Mathew <shaunmathew@gmail.com>
2026-06-29 20:30:10 +0530

    Build PDF upload API

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from .models import PDFDocument
+from .serializers import PDFDocumentSerializer
 
-# Create your views here.
+class PDFUploadView(CreateAPIView):
+    queryset = PDFDocument.objects.all()
+    serializer_class = PDFDocumentSerializer
